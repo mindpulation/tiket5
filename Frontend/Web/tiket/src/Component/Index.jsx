11a897a79c72de40b1f818912297e527de06e601
Reza Andriansyah <rezaxxxx02@gmail.com>
2019-12-28 22:59:04 +0700

    Create Container in Index(Progress)

--- Frontend/Web/tiket/src/Component/Index.jsx
+++ Frontend/Web/tiket/src/Component/Index.jsx
@@ -3,7 +3,16 @@ import React from 'react';
 export default function Index(){
     return(
         <div className="BodIndex">
-            
+            <div className="container">
+                <div className="box">
+                    <div className="row1">
+
+                    </div>
+                    <div className="row2">
+                        
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
