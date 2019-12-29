1f2a04e7cecd5c1cfcf6a2d1cc29d65f05f7f226
Reza Andriansyah <rezaxxxx02@gmail.com>
2019-12-29 13:57:52 +0700

    Create basic design for input form

--- Frontend/Web/src/Component/Index.jsx
+++ Frontend/Web/src/Component/Index.jsx
@@ -5,11 +5,63 @@ export default function Index(){
         <div className="BodIndex">
             <div className="container">
                 <div className="box">
-                    <div className="row1">
+                    <div className="group">
+                        <div className="row1">
+                            
+                            <p className="txt-title">
+                                Data Pribadi
+                            </p>
+                            
+                            <input type="number" />
+                            
+                            <input type="text" name="" id=""/>
+                            
+                            <input type="text" name="" id=""/>
+                            
+                            <select>
+                                <option value=""></option>
+                            </select>
 
-                    </div>
-                    <div className="row2">
-                        
+                            <select>
+                                <option value=""></option>
+                            </select>
+
+                            <select>
+                                <option value=""></option>
+                            </select>
+
+                            <select>
+                                <option value=""></option>
+                            </select>
+
+                            <input type="text" name="" id=""/>
+
+                            <select>
+                                <option> Kelas </option>
+                                <option value="10">10</option>
+                                <option value="11">11</option>
+                                <option value="12">12</option>
+                            </select>
+
+                            <select>
+                                <option> Jurusan </option>
+                                <option value="IPA">IPA</option>
+                                <option value="IPS">IPS</option>
+                                <option value="BAHASA">BAHASA</option>
+                            </select>
+
+                            <input type="number" name="" id=""/>
+
+                            <input type="email" name="" id=""/>
+
+                            <textarea name="" id="" cols="30" rows="10">
+                                
+                            </textarea>
+
+                        </div>
+                        <div className="row2">
+                            
+                        </div>
                     </div>
                 </div>
             </div>
