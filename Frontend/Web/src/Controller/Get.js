f415c6886261f4d2f895856802f3c8ba91917d1b
Reza Andriansyah <rezaxxxx02@gmail.com>
2020-01-08 03:45:49 +0700

    Create feature qr-code (generate & scan)

--- Frontend/Web/src/Controller/Get.js
+++ Frontend/Web/src/Controller/Get.js
@@ -6,4 +6,8 @@ export default function GetController(){
         return await axios.get("http://localhost:2020/get_all");
     }
 
+    this.ceReg = async (param) => {
+        return await axios.post("http://localhost:2020/check_noreg", param);
+    }
+
 }
